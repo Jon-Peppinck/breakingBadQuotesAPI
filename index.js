605082ea06d2bd720b7339f577f3ab1400594214
Jon Peppinck <mrjonpeppinck@gmail.com>
2019-01-31 23:23:02 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const quotes = require('./quotes.js');
+const ports = process.env.PORT || 3000;
+
+let quote;
+let previousQuote;
+
+app.get('/quotes', (req, res) => {
+  res.send(quotes);
+});
+
+app.get('/quote', (req, res) => {
+  quote = quotes[Math.floor(Math.random() * quotes.length)];
+  while (previousQuote == quote) {
+    quote = quotes[Math.floor(Math.random() * quotes.length)];
+  }
+  previousQuote = quote;
+  res.send(quote);
+});
+
+app.listen(ports, () => console.log(`listening on Port ${ports}`));
